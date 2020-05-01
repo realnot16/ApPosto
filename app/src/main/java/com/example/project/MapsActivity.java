7819637811caf9dd3967e73413bbec970b535890
GIACOMO CICCARELLI <S1068645@studenti.univpm.it>
2020-05-01 22:55:08 +0200

    Giacomo come sempre è hacker

--- app/src/main/java/com/example/project/MapsActivity.java
+++ app/src/main/java/com/example/project/MapsActivity.java
@@ -2,7 +2,11 @@ package com.example.project;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,9 +15,22 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private SearchView searchView;
+    private Integer filter_destination_meter=1200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Search bar che fa partire la ricerca dalla tastiera (settando imeOption=activitysearch su xml)
+        //e che risponde all'evento QueryTextSubmit sparando il metodo DbRequest che comunica con api google
+        // per la ricerca delle coordinate relative alla ricerca e contatta il nostro DB in cloud per farsi ritornare
+        //un json di parcheggi idonei
+        searchView=(SearchView) findViewById(R.id.searchview_id);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Aggiungere API google per ricavare coordinate della destinazione con la query
+                double lat_dest= 45.070841;//fittizie: dovrebbero essere ritornate da google api
+                double long_dest=7.668552;//fittizie
+                String city="Torino";//fittizia
+                ParametersAsync parametersAsync=new ParametersAsync(lat_dest,long_dest,city);
+                new DbAsyncRequest().execute(parametersAsync);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
 
@@ -44,4 +84,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    private class DbAsyncRequest extends AsyncTask<ParametersAsync,Void,Boolean>
+
+    {
+        @Override
+        protected Boolean doInBackground(ParametersAsync... parametersAsyncs) {
+
+            try {
+
+                URL url = new URL("http://smartparkingpolito.altervista.org/AvailableParking.php"); //
+                //preparazione della richiesta
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); //apertura connessione
+                urlConnection.setReadTimeout(1000);
+                urlConnection.setConnectTimeout(1500);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+                String lat_dest_string =String.valueOf(parametersAsyncs[0].latitude); //converto in stringhe i valori per inserirli nella richiesta
+                String long_dest_string =String.valueOf(parametersAsyncs[0].longitude);
+                String filter_string =String.valueOf(filter_destination_meter);
+
+
+                String params = "lat_destination=" +URLEncoder.encode(lat_dest_string, "UTF-8")
+                        +"&long_destination=" +URLEncoder.encode(long_dest_string, "UTF-8")
+                        +"&city="+URLEncoder.encode(parametersAsyncs[0].city, "UTF-8")//modificare:estrarre la città da preferenze
+                        +"&dist_filter_meter="+URLEncoder.encode(filter_string, "UTF-8");
+                Log.i("param", params);
+                DataOutputStream dos = new DataOutputStream(urlConnection.getOutputStream());
+                dos.writeBytes(params);
+                dos.flush();
+                dos.close();
+
+
+                urlConnection.connect(); //connessione
+                InputStream is = urlConnection.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+
+                String result = sb.toString();
+                Log.i("result", result);
+                if(result==null){
+                    String no_parking = getResources().getString(R.string.toast_no_parking);
+                    Log.i("param1", no_parking);
+                    Toast.makeText(getBaseContext(), no_parking,Toast.LENGTH_SHORT).show();
+
+
+                }
+                else {
+                    JSONArray jArray = new JSONArray(result);
+
+                    String outputString = "";
+                    for (int i = 0; i < jArray.length(); i++) {         //Ciclo di estrazione oggetti
+                        JSONObject json_data = jArray.getJSONObject(i);
+                        String latitudine=json_data.getString("latitude");
+                        Log.i("latitudine", latitudine);
+                        String longitudine=json_data.getString("longitude");
+                        Log.i("longitudine", longitudine);
+
+
+
+
+                    }
+                }
+
+
+            } catch (Exception e) {
+                Log.e("log_tag", "Error " + e.toString());
+            }
+            return null;
+        }
+
+    }
+
 }
