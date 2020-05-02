09d3bcb3a46e4bccf63ccc6dec712c7a7a953ccc
GIACOMO CICCARELLI <S1068645@studenti.univpm.it>
2020-05-02 10:24:37 +0200

    Giacomo come sempre è hacker

---
+++ app/src/main/java/com/example/project/ParametersAsync.java
@@ -0,0 +1,37 @@
+package com.example.project;
+
+public class ParametersAsync {
+    double latitude;
+    double longitude;
+    String city;
+
+    public ParametersAsync(double latitude, double longitude, String city) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.city = city;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
