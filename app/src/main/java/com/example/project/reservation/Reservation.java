af78a03de9723a0750697385988e73d0b6299a40
Flavia <flaviacostanza@gmail.com>
2020-05-18 11:38:25 +0200

    Storico altervista prova funzionante, vista dettagliata da completare

--- app/src/main/java/com/example/project/reservation/Reservation.java
+++ app/src/main/java/com/example/project/reservation/Reservation.java
@@ -1,6 +1,157 @@
 package com.example.project.reservation;
 
-class Reservation {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+class Reservation implements Parcelable {
+
+    private String id_booking;
+    private String time_start;
+    private String time_end;
+    private String address_start;
+    private int bonus;
+    private int successful;
+    private int parking_id;
+    private String user_id;
+    private float amount;
+
+    protected Reservation(Parcel in) {
+        id_booking = in.readString();
+        time_start = in.readString();
+        time_end = in.readString();
+        address_start = in.readString();
+        bonus = in.readInt();
+        successful = in.readInt();
+        parking_id = in.readInt();
+        user_id = in.readString();
+        amount = in.readFloat();
+    }
+
+    public static final Creator<Reservation> CREATOR = new Creator<Reservation>() {
+        @Override
+        public Reservation createFromParcel(Parcel in) {
+            return new Reservation(in);
+        }
+
+        @Override
+        public Reservation[] newArray(int size) {
+            return new Reservation[size];
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "Prenotazione n°: " + id_booking + "\n"+
+                "Data inizio: " + time_start + "\n"+
+                "Partenza da: " + address_start + "\n" +
+                "Importo pagato: " + amount ;
+    }
+
+    public void setId_booking(String id_booking) {
+        this.id_booking = id_booking;
+    }
+
+    public void setTime_start(String time_start) {
+        this.time_start = time_start;
+    }
+
+    public void setTime_end(String time_end) {
+        this.time_end = time_end;
+    }
+
+    public void setAddress_start(String address_start) {
+        this.address_start = address_start;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setSuccessful(int successful) {
+        this.successful = successful;
+    }
+
+    public void setParking_id(int parking_id) {
+        this.parking_id = parking_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+
+    public Reservation(String id_booking, String time_start, String time_end, String address_start, int bonus, int successful, int parking_id, String user_id, float amount) {
+        this.id_booking = id_booking;
+        this.time_start = time_start;
+        this.time_end = time_end;
+        this.address_start = address_start;
+        this.bonus = bonus;
+        this.successful = successful;
+        this.parking_id = parking_id;
+        this.user_id = user_id;
+        this.amount = amount;
+    }
+
+    public Reservation() {
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getId_booking() {
+        return id_booking;
+    }
+
+    public String getTime_start() {
+        return time_start;
+    }
+
+    public String getTime_end() {
+        return time_end;
+    }
+
+    public String getAddress_start() {
+        return address_start;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public int getSuccessful() {
+        return successful;
+    }
+
+    public int getParking_id() {
+        return parking_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id_booking);
+        parcel.writeString(time_start);
+        parcel.writeString(time_end);
+        parcel.writeString(address_start);
+        parcel.writeInt(bonus);
+        parcel.writeInt(successful);
+        parcel.writeInt(parking_id);
+        parcel.writeString(user_id);
+        parcel.writeFloat(amount);
+    }
 
     //Dati sui campi della tabella prenotazioni SQLite locale
     public static class ReservationMetaData {
@@ -13,6 +164,6 @@ class Reservation {
         public static String ID_PARKING = "id_parking";
         public static String USER_MAIL = "user_mail";
         public static String[] COLUMNS = new String[] { ID, TIME_START,TIME_FINISH, AMOUNT,
-        START_ADDRESS, FINISH_ADDRESS, ID_PARKING, USER_MAIL};
+                START_ADDRESS, FINISH_ADDRESS, ID_PARKING, USER_MAIL};
     }
 }
--- app/src/main/java/com/example/project/reservation/ReservationsActivity.java
+++ app/src/main/java/com/example/project/reservation/ReservationsActivity.java
@@ -3,16 +3,42 @@ package com.example.project.reservation;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.project.DetailedReservationActivity;
 import com.example.project.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReservationsActivity extends ListActivity {
 
-    private DBManager dbm;
     private static final String MY_SHARED_PREF = "login_prefs";
     private static final String EMAIL_DATA_KEY = "remember_mail";
+    private static final String TAG= "ReservationActivity";
+    private static final String URL_DB= "http://pmscflaviahosting.altervista.org/booking_php.php";
+    private List<Reservation> storico= new ArrayList<Reservation>();
+    private String mailUtente;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +46,121 @@ public class ReservationsActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservations);
 
-        //Creo un oggetto DBManager-> prende il riferimento al db o lo crea
-        //devo passare il Context ed il riferimento alla listview
-        dbm= new DBManager(getApplicationContext());
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long l) {
+                //qui scrivo cosa fare al click
+                Intent intent= new Intent(getApplicationContext(), DetailedReservationActivity.class);
+                Bundle reservationBundle =new Bundle();
+                Reservation selected= (Reservation)adapter.getItemAtPosition(pos);
+                Log.i(TAG, "HAI CLICCATO SULLA PRENOTAZIONE DI: "+selected.getUser_id());
+                reservationBundle.putParcelable("reservation", selected);
+                intent.putExtra("reservation", reservationBundle);
+                startActivity(intent);
+            }
+        });
 
-        SharedPreferences prefs = getSharedPreferences(MY_SHARED_PREF,Context.MODE_PRIVATE);
-        String mailUtente = prefs.getString(EMAIL_DATA_KEY, "No preferenze!");
+        //codice per recuperare utente da preferenze
+        SharedPreferences prefs = getSharedPreferences(MY_SHARED_PREF, Context.MODE_PRIVATE);
+        mailUtente = prefs.getString(EMAIL_DATA_KEY, "No preferenze!");
+        Log.i(TAG, "Utente salvato nelle preferenze:"+mailUtente.toString());
 
-        //chiamo il metodo che aggiorna la ListView, a cui devo passare la mail dell'utente
-        dbm.updateListView(getListView(), mailUtente);
+        updateReservationList();
+
     }
+
+    private void updateReservationList() {
+        DownloadReservationTask drt= new DownloadReservationTask();
+        drt.execute(URL_DB);
+    }
+
+    //Async Task
+    private class DownloadReservationTask extends AsyncTask<String, Void, List<Reservation>> {
+
+        @Override
+        protected List<Reservation> doInBackground(String... urls) {//ATTIVITA' CRITICA
+            return loadReservationsFromDB(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<Reservation> storico) {//QUANDO FINSCO ATTIVITA' CRITICA
+
+            ArrayAdapter a= new ArrayAdapter(getApplicationContext(), R.layout.reservation_simple_row_layout, R.id.textViewList, storico);
+            getListView().setAdapter(a);
+        }
+    }
+
+    private List<Reservation> loadReservationsFromDB(String urlstring) {
+        URL url;
+        storico.clear();
+        try {
+            url = new URL(urlstring);
+            //connessione
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+
+            //concateno al POST l'user_id per filtrare la tabella
+            String params = "user_id=" + URLEncoder.encode("flavia@gmail.com", "UTF-8");
+            //utente da preferenze
+            //String params = "user_id=" + URLEncoder.encode(mailUtente.toString(), "UTF-8");
+            DataOutputStream dos = new DataOutputStream(urlConnection.getOutputStream());
+            dos.writeBytes(params);
+            dos.flush();
+            dos.close();
+
+            urlConnection.connect();
+            InputStream is = urlConnection.getInputStream();
+
+            //4c)leggo i contenuti della pagina
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+
+            String result = sb.toString();//il risultato è una stringa Json
+
+            JSONArray jArray = new JSONArray(result); //decodifico la stringa Json
+            //per decodificare uso la classe jsonArray-> collezione di oggetti JsonObject
+            //su cui faccio un ciclo, ad ogni oggetto chiedo di darmi l'elemento corrispondente alle colonne della tabella
+
+            String outputString = "";
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject json_data = jArray.getJSONObject(i);
+                Log.i(TAG, "\n ID_BOOKING =  " + json_data.getString("id_booking") +
+                        "\n DATA INIZIO = " + json_data.getString("time_start") +
+                        "\n DATA FINE = " + json_data.getString("time_end") +
+                        "\n INDIRIZZO DI PARTENZA = " + json_data.getString("address_start") +
+                        "\n UTENTE = " + json_data.getString("user_id"));
+
+                //4d) ottenuto l'array di classe Json posso creare un oggetto Reservation e inserirlo nell'array
+                Reservation temp= new Reservation();
+                temp.setId_booking(json_data.getString("id_booking"));
+                temp.setTime_start(json_data.getString("time_start"));
+                temp.setTime_end(json_data.getString("time_end"));
+                temp.setAddress_start(json_data.getString("address_start"));
+                temp.setBonus(json_data.getInt("bonus"));
+                temp.setSuccessful(json_data.getInt("successful"));
+                temp.setParking_id(json_data.getInt("parking_id"));
+                temp.setUser_id(json_data.getString("user_id"));
+                temp.setAmount((float)json_data.getDouble("amount")); //per ora con un cast, poi si vede
+
+                storico.add(temp);
+                Log.i(TAG, "Utente inserito nell'array: "+ temp.toString());
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "Error*** " + e.toString());
+        }
+        return storico;
+
+    }
+
+
 }
