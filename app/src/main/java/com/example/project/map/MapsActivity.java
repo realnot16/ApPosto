31abdbd56f9d7089018aea2e203c84b86dd8b02e
realnot16 <benedetto.padula@studenti.polito.it>
2020-05-21 14:42:37 +0200

    Aggiunta geolocalizzazione

--- app/src/main/java/com/example/project/map/MapsActivity.java
+++ app/src/main/java/com/example/project/map/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.example.project.map;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.appcompat.widget.Toolbar;
@@ -11,6 +13,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,12 +25,19 @@ import android.widget.Toast;
 import com.example.project.R;
 import com.example.project.userManagement.Profilo;
 import com.example.project.userManagement.ShowProfile;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -55,56 +65,80 @@ import javax.net.ssl.HttpsURLConnection;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "Mappa";
+
+    //MAPPA
     private GoogleMap mMap;
-    private DrawerLayout drawerLayout;
-    private Integer filter_destination_meter=1200;
+    private CameraPosition mCameraPosition;
+    private PlacesClient mPlacesClient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private final LatLng mDefaultLocation = new LatLng(45.070841, 7.668552);
+    private static final int DEFAULT_ZOOM = 15;
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+    private boolean permissionChoose=false;
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location mLastKnownLocation;
+
+    // Keys for storing activity state.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+
+
+    //SCANNER
     private final static int  SCANNER_REQUEST_CODE=2;
     private final static int  PROFILE_REQUEST_CODE=1;
     private final static int MY_CAMERA_REQUEST_CODE=100;
     FloatingActionButton qrButton;
+
+    //AUTENTICAZIONE
     FirebaseAuth mAuth;
     FirebaseUser user;
+
+    //VARIE
+    private DrawerLayout drawerLayout;
+    private Integer filter_destination_meter=1200;  //FILTRO STAZIONI CARICATE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_layout_main);
+
+        //AUTENTICAZIONE
         mAuth = FirebaseAuth.getInstance();
 
-        // IMPOSTO LA TOOLBAR E LA COLLEGO AL NAVIGATION DRAWER
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true); // for add back arrow in action bar
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.map_ic_menu_black_24dp);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setTitle("");
-        setNavigationDrawer(toolbar);
-        Drawable filterIcon = getDrawable(R.drawable.map_ic_filter_list_black_24dp);
-        toolbar.setOverflowIcon(filterIcon);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // TOOLBAR E NAVIGATION DRAWER
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setToolbar(toolbar);
+
+
+        // MAPPA : Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        if (savedInstanceState != null) {  //Recupero informazioni sull'ultima posizione rilevata
+            mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
         final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
-        //IMPLEMENTO BOTTONE PER QR CODE ACTIIVTY
-        qrButton= findViewById(R.id.floatingQrButton);
-        qrButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MapsActivity.this,   //controlla che il permesso sia garantito
-                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(MapsActivity.this,   //richiesta di permesso all'utente
-                            new String[]{Manifest.permission.CAMERA},MY_CAMERA_REQUEST_CODE);
-                }
-                else{
-                    Intent intent=new Intent(MapsActivity.this, ScannerActivity.class);
-                    startActivityForResult(intent,SCANNER_REQUEST_CODE);
+        Places.initialize(getApplicationContext(),getResources().getString(R.string.google_maps_key));
+        mPlacesClient = Places.createClient(this);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-                }
-            }
-        });
+        // Retrieve location and camera position from saved instance state.
+
+
+        // BOTTONE PER QR CODE ACTIIVTY
+        qrButton= findViewById(R.id.floatingQrButton);
+        setQrButton(qrButton);
+
     }
 
     //When initializing your Activity, check to see if the user is currently signed in.
@@ -115,6 +149,29 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         user = mAuth.getCurrentUser();
         //mAuth.signOut();
         //updateUI(currentUser);
+    }
+
+    //ACTIVITY IN PAUSA
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //salvo lo stato della mappa quando l'activity è in pausa
+        if (mMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, mLastKnownLocation);
+            super.onSaveInstanceState(outState);
+        }
+    }
+
+    //IMPOSTO LA TOOLBAR
+    private void setToolbar(Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true); // for add back arrow in action bar
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.map_ic_menu_black_24dp);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("");
+        setNavigationDrawer(toolbar);
+        Drawable filterIcon = getDrawable(R.drawable.map_ic_filter_list_black_24dp);
+        toolbar.setOverflowIcon(filterIcon);
     }
 
     //COLLEGO IL NAVIGATION LAYOUT ALLA TOOLBAR
@@ -153,6 +210,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         actionBarDrawerToggle.syncState();
 
     }
+
 
     //Prendo i dati sul profilo dal DB
     private class LoadProfile extends AsyncTask<String,Void,Boolean> {
@@ -232,24 +290,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
-
-
-    //IMPOSTO LA MAPPA
-    private GoogleMap setMap(GoogleMap map) {
-        map.getUiSettings().setZoomControlsEnabled(true);
-        map.getUiSettings().setCompassEnabled(true);
-        //AGGIUNGERE ALTRE MODIFICHE -> POSIZIONE INIZIALE, ZOOM INIZIALE
-
-        // INIZIALIZZO POSIZIONE ATTUALE
-        LatLng start = new LatLng(45.0781,7.6761);
-        MarkerOptions tor = new MarkerOptions().position(start).title("TOpark");
-        mMap.addMarker(tor);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,16F));
-
-        return mMap;
-    }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -259,7 +299,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         Log.i(TAG,"mappa Pronta");
         mMap = googleMap;
-        mMap = setMap(mMap);
+        setMap();
 
         //CARICO LE STAZIONI
 
@@ -272,9 +312,96 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toast prova = Toast.makeText(this, "estrazione finita",Toast.LENGTH_SHORT);
         prova.show();
 
+
     }
 
 
+    //IMPOSTO LA MAPPA
+    private void setMap() {
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+
+        // Posizione di default
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+
+        //Acquisisco i permessi e  riposiziono la vista
+        getLocationPermission();
+
+        return;
+    }
+
+    //Acquisisco la posizione del dispositivo e riposiziono la vista
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        Log.i(TAG, "Acquisisco posizioneDevice");
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = task.getResult();
+                            if (mLastKnownLocation != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(mLastKnownLocation.getLatitude(),
+                                                mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+                            Log.i(TAG, "Posizione Acquisita");
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    //RICHIESTA PERMESSI GEOLOCALIZZAZIONE
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        Log.i(TAG,"Richiedo Permesso");
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    //IMPOSTO IL BOTTONE PER QR ACTIVITY
+    private void setQrButton(View qrButton) {
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MapsActivity.this,   //controlla che il permesso sia garantito
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MapsActivity.this,   //richiesta di permesso all'utente
+                            new String[]{Manifest.permission.CAMERA},MY_CAMERA_REQUEST_CODE);
+                }
+                else{
+                    Intent intent=new Intent(MapsActivity.this, ScannerActivity.class);
+                    startActivityForResult(intent,SCANNER_REQUEST_CODE);
+
+                }
+            }
+        });
+    }
 
     //LOAD STATION, da implementare
     //permette di acquisire tutte le stazioni nell'area visualizzata durante il primo accesso alla mappa
@@ -454,7 +581,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
+                                           @NonNull String[] permissions,@NonNull int[] grantResults) {
+
         switch (requestCode) {
             case MY_CAMERA_REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
@@ -468,6 +596,32 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
+            }
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                mLocationPermissionGranted = false;
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+                Log.i(TAG,"Esito permessi geoL: "+mLocationPermissionGranted);
+
+                Log.i(TAG,"Mi preparo: "+mLocationPermissionGranted); //Perché a runtime avviene prima di getLocalPermission?
+                if (mLocationPermissionGranted) {
+                    // Get the current location of the device and set the position of the map.
+                    Log.i(TAG, "Imposto posizioneAcquisita");
+                    getDeviceLocation();
+                    mMap.setMyLocationEnabled(true);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                }
+                else {
+                    Log.i(TAG, "Imposto posizioneDefault, Peremssi non accordati");
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                    mMap.setMyLocationEnabled(false);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                    mLastKnownLocation = null;
+                }
+
             }
 
             // other 'case' lines to check for other
